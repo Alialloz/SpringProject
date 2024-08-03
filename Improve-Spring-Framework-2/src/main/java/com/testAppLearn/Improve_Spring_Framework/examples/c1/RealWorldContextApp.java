b6acaa32da7b8ab4bb2c85aa40b7358f16fa3545
Alialloz <alialloz94450@gmail.com>
2024-08-03 19:14:06 -0400

    Create RealWorldContextApp.Java

---
+++ Improve-Spring-Framework-2/src/main/java/com/testAppLearn/Improve_Spring_Framework/examples/c1/RealWorldContextApp.java
@@ -0,0 +1,21 @@
+package com.testAppLearn.Improve_Spring_Framework.examples.c1;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+@ComponentScan //("com.testAppLearn.Improve_Spring_Framework.examples.a0")
+public class RealWorldContextApp {
+
+    // Beans automatiquement importés et par ComponentScan
+    public static void main (String[] args) {
+        try(AnnotationConfigApplicationContext context =
+                    new AnnotationConfigApplicationContext(RealWorldContextApp.class)) {
+
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+        }
+    }
+}
