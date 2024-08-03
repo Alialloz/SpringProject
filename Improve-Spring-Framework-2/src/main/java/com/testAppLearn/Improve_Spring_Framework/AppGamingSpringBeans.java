c3defc339aa937ae5937c2c0ed497b8848334d9b
Alialloz <alialloz94450@gmail.com>
2024-08-03 16:42:07 -0400

    Update AppGamingSpringBeans.java

--- Improve-Spring-Framework-2/src/main/java/com/testAppLearn/Improve_Spring_Framework/AppGamingSpringBeans.java
+++ Improve-Spring-Framework-2/src/main/java/com/testAppLearn/Improve_Spring_Framework/AppGamingSpringBeans.java
@@ -2,20 +2,29 @@ package com.testAppLearn.Improve_Spring_Framework;
 
 import com.testAppLearn.Improve_Spring_Framework.game.GameRunner;
 import com.testAppLearn.Improve_Spring_Framework.game.GamingConsole;
-import com.testAppLearn.Improve_Spring_Framework.game.MarioGame;
+import com.testAppLearn.Improve_Spring_Framework.game.SuperJeu;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppGamingSpringBeans {
 
+    @Bean
+    public GamingConsole game() {
+        return new SuperJeu();
+    }
+
+    @Bean
+    public GameRunner gameRunner(GamingConsole game) {
+        return new GameRunner(game);
+    }
+
     public static void main (String[] args) {
-
-
         try(AnnotationConfigApplicationContext context =
-                    new AnnotationConfigApplicationContext(GamingConfiguration.class)) {
+                    new AnnotationConfigApplicationContext(AppGamingSpringBeans.class)) {
             context.getBean(GamingConsole.class).up();
-
             context.getBean(GameRunner.class).run();
-
         }
     }
 }
