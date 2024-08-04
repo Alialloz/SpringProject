876e3af6250fdd4804c6cea2b7b54dfa97c093a1
Alialloz <alialloz94450@gmail.com>
2024-08-04 14:31:23 -0400

    Finalisation projet

--- learn-jpa-and-hibernate/src/main/java/com/codally/springboot/learn_jpa_and_hibernate/course/CourseCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/codally/springboot/learn_jpa_and_hibernate/course/CourseCommandLineRunner.java
@@ -2,6 +2,7 @@ package com.codally.springboot.learn_jpa_and_hibernate.course;
 
 import com.codally.springboot.learn_jpa_and_hibernate.course.jdbc.CourseJdbcRepository;
 import com.codally.springboot.learn_jpa_and_hibernate.course.jpa.CourseJpaRepository;
+import com.codally.springboot.learn_jpa_and_hibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,16 +13,27 @@ public class CourseCommandLineRunner implements CommandLineRunner {
     // @Autowired
     // private CourseJdbcRepository repository;
 
+    //@Autowired
+    //private CourseJpaRepository repository;
+
     @Autowired
-    private CourseJpaRepository repository;
+    private CourseSpringDataJpaRepository repository;
 
 
     @Override
     public void run(String... args) throws Exception {
-       repository.insert(new Course(1, "Learn AWS now!", "Codally"));
+       repository.save(new Course(1, "Learn AWS now!", "Codally"));
+       repository.save(new Course(1, "Learn Azure now!", "Ali"));
+       repository.save(new Course(1, "Learn Spring now!", "Morgan"));
 
-       //repository.delete(1);
+
+        //repository.delete(1);
 
         System.out.println(repository.findById(1L));
+        System.out.println(repository.findAll());
+        System.out.println(repository.count());
+
+        // NOT WORKING
+        //System.out.println(repository.findByName("Learn AWS now!"));
     }
 }
---
+++ learn-jpa-and-hibernate/src/main/java/com/codally/springboot/learn_jpa_and_hibernate/course/springdatajpa/CourseSpringDataJpaRepository.java
@@ -0,0 +1,11 @@
+package com.codally.springboot.learn_jpa_and_hibernate.course.springdatajpa;
+
+import com.codally.springboot.learn_jpa_and_hibernate.course.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CourseSpringDataJpaRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findByName(String name);
+}
